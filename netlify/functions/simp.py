bd02df7d8d356441991a5e962163213de56c0603
santy4747 <72089354+santy4747@users.noreply.github.com>
2025-09-10 02:35:23 +0530

    my commit

---
+++ netlify/functions/simp.py
@@ -0,0 +1,11 @@
+import json
+
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps({'message': 'Hello from Netlify Functions!'})
+    }
